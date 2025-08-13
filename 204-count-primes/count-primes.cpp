30932e09d8b794cbbc587fbe68eeb0e5e2c66fc9
RahulPandey941 <rahul941pandey@gmail.com>
2025-08-13 14:26:46 +0530

    Time: 243 ms (23.84%) | Memory: 12.7 MB (42.01%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -3,7 +3,7 @@ public:
     int countPrimes(int n) {
         int count = 0 ;
         vector <bool> prime (n , true);
-        if(n <= 2) return 0;
+       
         
         for(int i = 2 ; i < n ; i++){
             
