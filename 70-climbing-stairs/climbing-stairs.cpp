569d3c4cd35b6fbabddcc17981574fec2c02c37b
RahulPandey941 <rahul941pandey@gmail.com>
2025-10-25 14:48:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (35.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<2) return n;
+        if(n<=2) return n;
         vector<int>sum(n+1);
-        sum[1] = 1;
-        sum[2] = 2;
+        int result;
+        int pre = 1;
+        int now = 2;
         for (int i = 3; i <= n; i++) {
-            sum[i] = sum[i-1] + sum[i-2];
+            result = now + pre;
+            pre = now;
+            now = result;
         }
-    return sum[n];
+    return result;
     }
 };
