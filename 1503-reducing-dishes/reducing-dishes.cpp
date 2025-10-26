f34de8a5f389d8872ae92d71dd5a364f2c9a4b69
RahulPandey941 <rahul941pandey@gmail.com>
2025-10-26 20:22:07 +0530

    Time: 3 ms (61.10%) | Memory: 11.5 MB (86.83%) - LeetSync

---
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin() , satisfaction.end());
+        int size = satisfaction.size();
+        int maxSum = 0;
+        for(int i = size-1; i>= 0 ; i--){
+            int sum = 0;
+            int counter = 1;
+            for(int j = i; j<size;j++){
+                sum += satisfaction[j] * counter++ ;
+            }
+            maxSum = max(sum , maxSum);
+        }
+        return maxSum;
+    }
+};
